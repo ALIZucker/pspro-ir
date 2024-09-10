7f6cbe6b79e049a8b23c6c8034fc1134e4efffbd
ALI_Zucker <zabihiali1998@gmail.com>
2024-09-10 09:55:53 +0330

    start routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterModule, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "../component/header/header.component";
+import {FooterComponent} from "../component/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, HeaderComponent],
+  imports: [RouterOutlet, RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
